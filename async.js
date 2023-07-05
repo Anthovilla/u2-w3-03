397ca86be556129619aab6500195f1c1a2e330b0
Anthovilla <9anthovilla7@gmail.com>
2023-07-05 22:40:00 +0200

    cards sistemati

--- async.js
+++ async.js
@@ -20,14 +20,14 @@ const libreria = function (){
         cards.classList.add('col')
         cards.innerHTML = `
 
-        <div class="card h-100">
-        <img src="${item.img}" class="card-img-top " alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${item.title}</h5>
+        <div class="card h-100 text-white">
+        <img src="${item.img}" class="card-img-top h-50" alt="...">
+        <div class="card-body d-flex flex-column justify-content-evenly align-items-center">
+          <h6 class="card-title">${item.title}</h6>
           <p class="card-text">${item.price}</p>
         </div>
         <div class="d-grid gap-2 col-6 mx-auto">
-<button class="btn btn-primary my-3" type="button"> Scarta </button>
+<button class="btn btn-primary mb-3" type="button"> Scarta </button>
  </div>
       </div>
 
